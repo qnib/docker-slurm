6a87519d6d2a1ebd4d8cc60d1ee52f5c01089f41
Christian Kniep <christian@qnib.org>
2015-04-07 11:55:12 +0200

    hard restart of slurmd (should introduce no harm)

--- opt/qnib/bin/slurm_reread.sh
+++ opt/qnib/bin/slurm_reread.sh
@@ -4,6 +4,7 @@ if [ -f /var/run/slurmctld.pid ];then
     kill -HUP $(cat /var/run/slurmctld.pid)
 fi
 if [ -f /var/run/slurmd.pid ];then
-    kill -HUP $(cat /var/run/slurmd.pid)
+    #kill -HUP $(cat /var/run/slurmd.pid)
+    supervisorctl restart slurmd
 fi
 sleep 5
