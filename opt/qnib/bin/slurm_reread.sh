621c1241bc5673f3f4c52ffeb31c25eae6836c45
Christian Kniep <christian@qnib.org>
2015-04-07 11:43:09 +0200

    changed signal to send to slurm deamon

--- opt/qnib/bin/slurm_reread.sh
+++ opt/qnib/bin/slurm_reread.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ -f /var/run/slurmctld.pid ];then
-    kill -SIGHUP $(cat /var/run/slurmctld.pid)
+    kill -HUP $(cat /var/run/slurmctld.pid)
 fi
 if [ -f /var/run/slurmd.pid ];then
-    kill -SIGHUP $(cat /var/run/slurmd.pid)
+    kill -HUP $(cat /var/run/slurmd.pid)
 fi
 sleep 5
