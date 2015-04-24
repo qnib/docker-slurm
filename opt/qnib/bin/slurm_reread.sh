a5a7fc54952b6e0f6e33be2ed5fb6d30aa33022b
Christian Kniep <christian@qnib.org>
2015-04-24 22:26:00 +0200

    odd/even partitions

--- opt/qnib/bin/slurm_reread.sh
+++ opt/qnib/bin/slurm_reread.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+odd_cnt=$(grep "^PartitionName=odd" /usr/local/etc/slurm.conf|grep -o ","|wc -l)
+even_cnt=$(grep "^PartitionName=even" /usr/local/etc/slurm.conf|grep -o ","|wc -l)
+rm -f /tmp/slurm.conf
+if [ ${odd_cnt} -le ${even_cnt} ];then
+    mv /usr/local/etc/slurm.conf /tmp/slurm.conf
+    grep -v ^PartitionName /tmp/slurm.conf > /usr/local/etc/slurm.conf
+    grep "^PartitionName=even" /tmp/slurm.conf >> /usr/local/etc/slurm.conf
+    grep "^PartitionName=odd" /tmp/slurm.conf|sed -e 's/Default=YES/Default=NO/' >> /usr/local/etc/slurm.conf
+else
+    mv /usr/local/etc/slurm.conf /tmp/slurm.conf
+    grep -v ^PartitionName /tmp/slurm.conf > /usr/local/etc/slurm.conf
+    grep "^PartitionName=even" /tmp/slurm.conf|sed -e 's/Default=YES/Default=NO/' >> /usr/local/etc/slurm.conf
+    grep "^PartitionName=odd" /tmp/slurm.conf >> /usr/local/etc/slurm.conf
+fi
+## check if partition is empty?
+sed -i '/\ Nodes=\ /d' /usr/local/etc/slurm.conf
 if [ -f /var/run/slurmctld.pid ];then
     kill -HUP $(cat /var/run/slurmctld.pid)
 fi
