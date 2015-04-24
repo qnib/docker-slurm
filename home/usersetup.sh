68ce024057a2541bcaf2e0a2d0e4e54350e6e110
Christian Kniep <christian@qnib.org>
2015-04-24 13:57:57 +0200

    chown forgotten

--- home/usersetup.sh
+++ home/usersetup.sh
@@ -4,8 +4,8 @@ for u in $*;do
     cp /tmp/home/bashrc /home/${u}/.bashrc && chown ${u}: /home/${u}/.bashrc
     cp /tmp/home/bash_profile /home/${u}/.bash_profile && chown ${u}: /home/${u}/.bash_profile
     mkdir -p /home/${u}/.ssh/
-    cp /tmp/home/id_rsa /home/${u}/.ssh/id_rsa && chmod 600 /home/${u}/.ssh/id_rsa
-    cp /tmp/home/id_rsa.pub /home/${u}/.ssh/id_rsa.pub && chmod 644 /home/${u}/.ssh/id_rsa.pub
-    cp /home/${u}/.ssh/id_rsa.pub /home/${u}/.ssh/authorized_keys
+    cp /tmp/home/id_rsa /home/${u}/.ssh/id_rsa && chmod 600 /home/${u}/.ssh/id_rsa && chown ${u}: /home/${u}/.ssh/id_rsa
+    cp /tmp/home/id_rsa.pub /home/${u}/.ssh/id_rsa.pub && chmod 644 /home/${u}/.ssh/id_rsa.pub && chown ${u}: /home/${u}/.ssh/id_rsa.pub
+    cp /home/${u}/.ssh/id_rsa.pub /home/${u}/.ssh/authorized_keys && chown ${u}: /home/${u}/.ssh/authorized_keys
 done
 
